bc1fc7f3a93adee37d6d9503a78a84ca09ef0c82
Sithum <99238978+IT21239298@users.noreply.github.com>
2024-05-04 01:34:09 +0530

    bug fixed

--- frontend/src/services/redux/store.js
+++ frontend/src/services/redux/store.js
@@ -4,7 +4,7 @@ import authReducer from "../../auth/redux/authSlice";
 import { authApi } from "../../auth/services/authService";
 import productSlideReducer from "../../services/redux/productSlice";
 import { checkoutSlice } from "./checkoutSlice";
-
+import { apiSlice } from "../../components/Store/apiSlice";
 
 
 const store = configureStore({
@@ -14,6 +14,7 @@ const store = configureStore({
    
     [authApi.reducerPath]:authApi.reducer,
     [checkoutSlice.reducerPath]:checkoutSlice.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
     
   },
   middleware: (getDefaultMiddleware) =>
